a945f43e5fca89f259e223afd753ffe3a502f6e1
Muj-90 <80159717+Muj-90@users.noreply.github.com>
2022-01-18 09:25:39 +0000

    updata_homework

--- week_05/day_03/5_homework/shiny_hw/my_app.R
+++ week_05/day_03/5_homework/shiny_hw/my_app.R
@@ -7,24 +7,24 @@ library(tidyverse)
 
 recovery_times <- read_csv("recovery_times.csv")
 ui <- fluidPage(
-  theme = shinytheme("cerulean"),
-  titlePanel("recovery times"),
+  theme = shinytheme("united"),
+  titlePanel("Recovery times أوقات الشفاء "),
   
   sidebarLayout(
     sidebarPanel(
       
       radioButtons("treatment_input",
-                   "what type of treatment?",
+                   "what type of treatment ما نوع العلاج?",
                    choices = c("treatment_a", "treatment_b","control")
       ),
       
       selectInput("team_input",
-                  "what is the result?",
+                  "what is the result ما هي النتيجة?",
                   choices = c("",
-                              "Excellent",
-                              "Good",
-                              "Average",
-                              "Poor"
+                              "Excellent ممتاز",
+                              "Good حسن",
+                              "Average متوسط",
+                              "Poor فقير"
                   )
       )
     ),
@@ -40,9 +40,9 @@ server <- function(input, output) {
     
     recovery_times %>%
       filter(prognosis == "prognosis") %>%
-      filter(treatment_group == input$treatment_group_input) %>%
+      filter(treatment_group == input$treatment) %>%
       ggplot() +
-      aes(x = treatment_group, y = recovery, fill = prognosis) +
+      aes(x = treatment_group, y = recovery, fill = treatment_group) +
       geom_col()
   })
 }

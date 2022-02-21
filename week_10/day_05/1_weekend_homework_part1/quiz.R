4685a6bf38200f69486f71f4f3caecb3204da355
Muj-90 <80159717+Muj-90@users.noreply.github.com>
2022-02-21 09:26:41 +0000

    updata

---
+++ week_10/day_05/1_weekend_homework_part1/quiz.R
@@ -0,0 +1,21 @@
+I want to predict how well 6 year-olds are going to do in their final school exams. Using the following variables am I likely under-fitting, fitting well or over-fitting? Postcode, gender, reading level, score in maths test, date of birth, family income.
+
+
+
+
+
+
+2 - If I have two models, one with an AIC score of 34,902 and the other with an AIC score of 33,559 which model should I use?
+  
+The better AIC is 33,559 
+
+
+
+
+3- I have two models, the first with: r-squared: 0.44, adjusted r-squared: 0.43. The second with: r-squared: 0.47, adjusted r-squared: 0.41. Which one should I use?
+  i will use r-squared: 0.44, adjusted r-squared: 0.43.
+
+
+
+
+4- I have a model with the following errors: RMSE error on test set: 10.3, RMSE error on training data: 10.4. Do you think this model is over-fitting?
